61c7e3d05ee3d3cb9bb87238ca43f256f13b0031
wesll <wesll@projectc.com>
2026-08-08 18:44:01 +0200

    fix: remove duplicate useLanguage export in LanguageProvider

--- src/components/LanguageProvider.js
+++ src/components/LanguageProvider.js
@@ -37,9 +37,3 @@ export function useLanguage() {
   if (!ctx) throw new Error('useLanguage must be used within LanguageProvider');
   return ctx;
 }
-
-export function useLanguage() {
-  const ctx = useContext(LanguageContext);
-  if (!ctx) throw new Error('useLanguage must be used within LanguageProvider');
-  return ctx;
-}
